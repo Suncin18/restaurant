f8db702a47a182ac8dbd9c3464677f51411ee346
Christian Suncin <SunciNx10@gmail.com>
2021-09-27 20:37:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// const btnNav = document.getElementById('nav-menu');
+const btnFilter = document.querySelectorAll('.btnJS');
+const menuItem = document.querySelectorAll('.menu_items');
+const menu = document.querySelector('.div_menu');
+
+// Botón del nav
+document.addEventListener('click', (e)=>{
+    if(e.target.closest('#nav-menu')){
+        menu.classList.toggle('ocultarMenu');
+        if(!menu.classList.contains('ocultarMenu')){
+        }
+    }else{
+        menu.classList.add('ocultarMenu');
+    }
+});
+
+// Filtrar menú
+btnFilter.forEach(btn => {
+    let className = btn.id;    
+    btn.addEventListener('click', ()=>{
+        menuItem.forEach(item => {
+            if(className != 'all'){
+                if(!item.classList.contains(className)){
+                    item.classList.add('ocultar');
+                }else{
+                    item.classList.remove('ocultar');
+                }
+            }else{
+                item.classList.remove('ocultar');
+            }
+        });
+    });
+});
